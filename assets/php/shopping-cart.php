fe8aa23d8ecd43571340ca6254d430e138d3199d
Feys Dylan <148909357+HappyFeys@users.noreply.github.com>
2024-06-12 11:25:13 +0200

    modif json + creation cart dans la shopping list

--- assets/php/shopping-cart.php
+++ assets/php/shopping-cart.php
@@ -21,6 +21,27 @@
 
     <main>
 
+        <?php   
+        $productsJson = file_get_contents('../../data.json');
+        $products = json_decode($productsJson, true);
+        foreach ($products as $product) {
+            echo '
+                <div class="shop__card">
+                    <img src="' . $product['image_url'] . '" alt="' . $product['product'] . '">
+                    <div class="card__details">
+                        <button>Add to cart</button>
+                        <div class="card__details--name">
+                            <p class="detail__name">' . $product['product'] . '</p>
+                            <p class="detail__price">' . $product['price'] . '€</p>
+                        </div>
+                    </div>
+                </div>
+                ';
+        }
+        ?>
+
+
+        
     </main>
 
     <?php require('./partials/footer.php') ?>
