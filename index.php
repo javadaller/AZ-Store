4290ae433fc88424600c96167d1b258231186237
javadaller <arnaudvanacker@yahoo.fr>
2024-06-12 09:25:37 +0200

    structure

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en" data-theme="light">
+
+    <head>
+
+        <title>AZ-Store</title>
+
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <link rel="stylesheet" type="text/css" href="assets/css/style.css">
+        <!--<link rel="icon" type="svg" href="assets/images/icons/calendar-todo-line.svg">-->
+        
+    </head>
+
+<body>
+
+    <header>
+        
+    </header>
+
+    <main>
+
+    </main>
+
+    <footer>
+
+    </footer>
+
+</body>
+
+<script type="module" src="assets/js/main.js"></script>
+
+</html>
