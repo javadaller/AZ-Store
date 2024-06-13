fc48141131966e62b98a365a09b272ef125c0531
Arnaud Van Acker <arnaudvanacker@yahoo.fr>
2024-06-13 10:04:38 +0200

    css homepage

--- assets/php/partials/home.php
+++ assets/php/partials/home.php
@@ -16,8 +16,32 @@
     <h3><span class="blue">Our</span> last products</h3>
 
 
-    <!-- LATEST PRODUCTS -->
+    <?php   
+    $productsJson = file_get_contents('../../data.json');
+    $products = json_decode($productsJson, true);
 
+    if (is_array($products)) {
+        // Prenez les 4 derniers éléments du tableau
+        $lastFourProducts = array_slice($products, -4);
+
+        foreach ($lastFourProducts as $product) {
+            echo '
+                <div class="shop__card">
+                    <img src="' . $product['image_url'] . '" alt="' . $product['product'] . '">
+                    <div class="card__details">
+                        <div class="card__details--name">
+                            <p class="detail__name">' . $product['product'] . '</p>
+                            <p class="detail__price">' . $product['price'] . '€</p>
+                        </div>
+                        <button>Add to cart</button>
+                    </div>
+                </div>
+                ';
+        }
+    } else {
+        echo 'Erreur: le fichier JSON ne contient pas un tableau valide.';
+    }
+    ?>
 
     <section id="bestQuality">
         <div id="logo2"></div>
