b9af286d0285198fc1418f6492e1e874301da024
Arnaud Van Acker <arnaudvanacker@yahoo.fr>
2024-06-13 11:39:43 +0200

    about

--- assets/php/about.php
+++ assets/php/about.php
@@ -1,7 +1,32 @@
 <?php require('./partials/start.php') ?>
 
     <main>
+        <section id="about">
+            <h2 class="teamTitle">OUR <span class="blue">TEAM</span>.</h2>
 
+            <h3 class="teamName">ISABELLE</h3>
+            <div class="socials">
+                <img class="github" src="../images/icons/github-line.svg" alt="github">
+                <img class="linkedin" src="../images/icons/linkedin.svg" alt="linkedin">
+            </div>
+            
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptate nisi minima nesciunt nihil vel, maxime id esse harum quod natus illo architecto ipsum adipisci at dicta, accusantium rerum illum saepe.</p>
+
+            <h3 class="teamName">DYLAN</h3>
+            <div class="socials">
+                <img class="github" src="../images/icons/github-line.svg" alt="github">
+                <img class="linkedin" src="../images/icons/linkedin.svg" alt="linkedin">
+            </div>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod ea nesciunt laudantium ipsum suscipit est veritatis! Quisquam, eius quam. Provident ex laboriosam dignissimos harum voluptatem. Exercitationem libero facilis temporibus provident!</p>
+
+            <h3 class="teamName">ARNAUD</h3>
+            <div class="socials">
+                <img class="github" src="../images/icons/github-line.svg" alt="github">
+                <img class="linkedin" src="../images/icons/linkedin.svg" alt="linkedin">
+            </div>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique molestiae ratione incidunt quos, perspiciatis aspernatur totam maxime blanditiis? Incidunt non nostrum velit omnis porro aperiam doloremque similique temporibus in voluptatum.</p>
+        </section>
+        
     </main>
 
 <?php require('./partials/end.php') ?>
